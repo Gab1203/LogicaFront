74fc75aeee607b26cec79227f36f8ad41a0e1966
GabrielFraga <gabrielbff2006@gmail.com>
2024-11-10 20:56:20 -0300

    Update q17.js

--- q17.js
+++ q17.js
@@ -5,18 +5,18 @@ exemplo, ao receber "javascript", deve retornar "tpircsavaj". */
 var result = document.querySelector('#result')
 
 function inverter(a){
+    
 
-    let reverse = ''
+    var reverse = a
 
-    for(let i = a.length; i >= 0; i++){
-        reverse += a.charAt(i)
-    }
-    return reverse
+    
+    return reverse.split("").reverse().join("");
 
 }
 
 
-var a = prompt('Digite uma string:')
+let a = prompt('Digite uma string:')
 var inverso = inverter(a)
+
 
 result.innerHTML = `String digitada: ${a} <br>String invertida: ${inverso}`
